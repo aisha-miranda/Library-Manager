a9fd89740b35b90ede69da3103d3cb415288703f
aisha-miranda <tashchanova@bk.ru>
2025-07-01 21:03:10 +0500

    Created class Book that declares all properties of the books to be supplied to the library - id, title, author, year, user availability

---
+++ src/Book.java
@@ -0,0 +1,37 @@
+public class Book {
+    private static int counter = 1;
+    private int id;
+    private String title;
+    private String author;
+    private int year;
+    private boolean isAvailable;
+
+    public Book(String title, String author, int year) {
+        this.id = counter++;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.isAvailable = true;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + id + "] " + title + " (" + author + ", " + year + ") - " + (isAvailable ? "Доступна" : "Выдана");
+    }
+}
